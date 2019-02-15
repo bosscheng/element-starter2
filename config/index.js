4574a782bf2c9b5ea650f7169e30b71e7b82280f
bosscheng <25022549@qq.com>
2019-02-15 17:49:52 +0800

    add files
    add files

--- config/index.js
+++ config/index.js
@@ -37,11 +37,11 @@ module.exports = {
     build: {
         //
         index: path.resolve(__dirname, '../dist/index.html'),
-
+        //
         assetsRoot: path.resolve(__dirname, '../dist'),
-
+        //
         assetsSubDirectory: 'static',
-
+        //
         assetsPublicPath: './'
     }
 }
